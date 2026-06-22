a3d8176330ed812791a6834cf9e1431931d925d2
davidaguiar-ug <david.aguiar@ug.uchile.cl>
2026-06-21 20:48:07 -0400

    Movimiento basico coleccionables

--- gameplay/collectible.gd
+++ gameplay/collectible.gd
@@ -1,6 +1,16 @@
 extends Area3D
 
+@export var rotation_speed := 12.0
+@export var float_speed := 3.0
+@export var float_amplitude: float = 0.3
+
+var target_angle := 0.0
+var angle: float = 0.0
+var start_y: float
+
+
 func _ready() -> void:
+	start_y = global_position.y
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node3D) -> void:
@@ -8,3 +18,11 @@ func _on_body_entered(body: Node3D) -> void:
 	if player:
 		monitoring = false
 		get_tree().reload_current_scene()
+		
+func _physics_process(delta: float) -> void:
+	target_angle += 3 * delta
+	rotation.y = lerp_angle(rotation.y, target_angle, rotation_speed * delta)
+	
+	angle += float_speed * delta
+	global_position.y = start_y + (sin(angle) * float_amplitude)	
+	
